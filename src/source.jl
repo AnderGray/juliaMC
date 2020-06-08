c06c08334c1c8c9d435740069376cf473064993b
Cyrus Wyett <34195737+cjwyett@users.noreply.github.com>
2020-06-08 01:56:23 +0100

    Update source.jl

--- src/source.jl
+++ src/source.jl
@@ -7,8 +7,8 @@
 @with_kw struct Source
 
     E_distribution :: Sampleable = Truncated(Normal(14.2e7,6e6),1e6+1,3e8-1)   # The distribution of neutron energies, default set to normal
-    position :: Array{Float64,1} = [0,0,0]                        # Position Where neutron is born, default is origin
-    direction_Inclination :: Sampleable = Uniform(0,2*pi)          # Spherical coordinates for direction, the default is an
+    position :: Array{Float64,1} = [0,0,0]                        # Position where neutron is born, default is origin
+    direction_Inclination :: Sampleable = Uniform(0,2*pi)          # Spherical coordinates for direction, the default is an ??
     direction_Azamuthal :: Sampleable = Uniform(0,1)             # isotropic distribution
 
 end
@@ -16,7 +16,7 @@ end
 
 
 
-# The generate function will generate a particle bank of length n from a source distribution s. This function could be altered later to also generate secondary neutrons to be added to an existing particel bank
+# The generate function will generate a particle bank of length n from a source distribution s. This function could be altered later to also generate secondary neutrons to be added to an existing particle bank
 
 @everywhere function generate(s :: Source, mat :: Material_Tendl, n :: Int64, grid :: Array{Float64,1})
 
@@ -34,7 +34,7 @@ end
 
     directions[:,1] = sin.(d_Az) .* cos.(d_Inc)     # Conversion of the spherical coordinates that have been sampled to the
     directions[:,2] = sin.(d_Az) .* sin.(d_Inc)     # carteesian used by particle. Here we have done this by vectorization
-    directions[:,3] = cos.(d_Az)                   # but is debatable if this is fast than looping in julia
+    directions[:,3] = cos.(d_Az)                   # but is debatable if this is faster than looping in Julia (usually not)
 
     for i = 1:n
         bank[i].E = Energys[i]                      # Filling the particle bank with sampled information.
